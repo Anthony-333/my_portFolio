af35b39b9b56cdf4dbe526fb5370715d49f57ae3
Anthony-333 <anthonyoling333@gmail.com>
2021-07-31 20:08:10 +0800

    commit on main

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,96 +22,98 @@ import "aos/dist/aos.css"; // You can also use <link> for styles
 AOS.init();
 
 AOS.init({
-
   once: false, // whether animation should happen only once - while scrolling down
   mirror: true, // whether elements should animate out while scrolling past them
-
 });
 
 function Main() {
   return (
     <div className="Main_body_wrapper">
       <div className="Main_body_wrapper_padding">
-        <div className="Social_Links_icons">
-          <div className="Social_Links_icons_div">
-            <FaFacebookF />
-          </div>
-          <div className="Social_Links_icons_div">
-            <FaTwitter />
-          </div>
-          <div className="Social_Links_icons_div">
-            <FaGithub />
-          </div>
-          <div className="Social_Links_icons_div">
-            <FaLinkedinIn />
-          </div>
-        </div>
-
-        <div className="Main_body_text">
-          <div className="Main_body_text_padding">
-            <h1
-              className="Welcome_text_1"
-              data-aos="fade-up"
-              data-aos-duration="2000"
-             
-            >
-              Hi,
-            </h1>
-            <h1 className="Welcome_text_2">I'm&nbsp;Anthony</h1>
-            <div className="Rubber_band_div">
-              <h1 className="Welcome_text_3">F</h1>
-              <h1 className="Welcome_text_3">u</h1>
-              <h1 className="Welcome_text_3">l</h1>
-              <h1 className="Welcome_text_3">l</h1>
-              <h1 className="Welcome_text_3">&nbsp;</h1>
-              <h1 className="Welcome_text_3">S</h1>
-              <h1 className="Welcome_text_3">t</h1>
-              <h1 className="Welcome_text_3">a</h1>
-              <h1 className="Welcome_text_3">c</h1>
-              <h1 className="Welcome_text_3">k</h1>
-              <h1 className="Welcome_text_3">&nbsp;</h1>
-              <h1 className="Welcome_text_3">D</h1>
-              <h1 className="Welcome_text_3">e</h1>
-              <h1 className="Welcome_text_3">v</h1>
+        <section className="One">
+          <div className="Social_Links_icons">
+            <div className="Social_Links_icons_div">
+              <FaFacebookF />
             </div>
-            <div className="Modeller">
-              <h1 className="Welcome_text_2">And&nbsp;</h1>
+            <div className="Social_Links_icons_div">
+              <FaTwitter />
+            </div>
+            <div className="Social_Links_icons_div">
+              <FaGithub />
+            </div>
+            <div className="Social_Links_icons_div">
+              <FaLinkedinIn />
+            </div>
+          </div>
+
+          <div className="Main_body_text">
+            <div className="Main_body_text_padding">
+              <h1
+                className="Welcome_text_1"
+                data-aos="fade-up"
+                data-aos-duration="2000"
+              >
+                Hi,
+              </h1>
+              <h1 className="Welcome_text_2">I'm&nbsp;Anthony</h1>
               <div className="Rubber_band_div">
-                <h1 className="Welcome_text_3">3</h1>
-                <h1 className="Welcome_text_3">D</h1>
+                <h1 className="Welcome_text_3">F</h1>
+                <h1 className="Welcome_text_3">u</h1>
+                <h1 className="Welcome_text_3">l</h1>
+                <h1 className="Welcome_text_3">l</h1>
                 <h1 className="Welcome_text_3">&nbsp;</h1>
-                <h1 className="Welcome_text_3">M</h1>
-                <h1 className="Welcome_text_3">o</h1>
-                <h1 className="Welcome_text_3">d</h1>
+                <h1 className="Welcome_text_3">S</h1>
+                <h1 className="Welcome_text_3">t</h1>
+                <h1 className="Welcome_text_3">a</h1>
+                <h1 className="Welcome_text_3">c</h1>
+                <h1 className="Welcome_text_3">k</h1>
+                <h1 className="Welcome_text_3">&nbsp;</h1>
+                <h1 className="Welcome_text_3">D</h1>
                 <h1 className="Welcome_text_3">e</h1>
-                <h1 className="Welcome_text_3">l</h1>
-                <h1 className="Welcome_text_3">l</h1>
-                <h1 className="Welcome_text_3">e</h1>
-                <h1 className="Welcome_text_3">r</h1>
+                <h1 className="Welcome_text_3">v</h1>
+              </div>
+              <div className="Modeller">
+                <h1 className="Welcome_text_2">And&nbsp;</h1>
+                <div className="Rubber_band_div">
+                  <h1 className="Welcome_text_3">3</h1>
+                  <h1 className="Welcome_text_3">D</h1>
+                  <h1 className="Welcome_text_3">&nbsp;</h1>
+                  <h1 className="Welcome_text_3">M</h1>
+                  <h1 className="Welcome_text_3">o</h1>
+                  <h1 className="Welcome_text_3">d</h1>
+                  <h1 className="Welcome_text_3">e</h1>
+                  <h1 className="Welcome_text_3">l</h1>
+                  <h1 className="Welcome_text_3">l</h1>
+                  <h1 className="Welcome_text_3">e</h1>
+                  <h1 className="Welcome_text_3">r</h1>
+                </div>
+              </div>
+
+              <div className="Main_project">
+                <Scrollspy>
+                  <p>Projects</p>
+                </Scrollspy>
+
+                <FaArrowDown className="arrow_down" />
               </div>
             </div>
-
-            <div className="Main_project">
-              <Scrollspy>
-                <p>Projects</p>
-              </Scrollspy>
-
-              <FaArrowDown className="arrow_down" />
-            </div>
           </div>
+
+          <div className="Main_body_model">
+            <img src={Myself} alt="" />
+          </div>
+        </section>
+
+        <div className="Section_two">
+          <section className="Two"></section>
         </div>
 
-        <div className="Main_body_model">
-          <img src={Myself} alt="" />
+        <div className="Section_Three">
+           <section className="Three">
+          
+        </section>
         </div>
-
-        
-
-
-
-
-
-
+       
       </div>
     </div>
   );
--- src/components/Rightsidebar.js
+++ src/components/Rightsidebar.js
@@ -6,45 +6,119 @@ import arrowDown from "react-useanimations/lib/arrowDown";
 
 import Circle from "./img/circle.png";
 
+import $ from "jquery";
+
 function Rightsidebar() {
+
+  
+  $(function() {
+    $(window).scroll(function() {
+      updateNav1();
+    });
+
+    updateNav1();
+
+    function updateNav1() {
+      $(".One").each(function(index) {
+        var topOffset = $(this).offset().top;
+        if (topOffset - $(window).scrollTop() < 50) {
+          $(".Marker_circle1").addClass("active");
+          $(".Marker_text1").addClass("Marker_text_active");
+        } else {
+          $(".Marker_circle1").removeClass("active");
+          $(".Marker_text1").removeClass("Marker_text_active");
+        }
+      });
+    }
+  });
+
+  $(function() {
+    $(window).scroll(function() {
+      updateNav2();
+    });
+
+    updateNav2();
+
+    function updateNav2() {
+      $(".Two").each(function(index) {
+        var topOffset = $(this).offset().top;
+        if (topOffset - $(window).scrollTop() < 50) {
+          $(".Marker_circle1").removeClass("active");
+          $(".Marker_text1").removeClass("Marker_text_active");
+
+          $(".Marker_circle2").addClass("active");
+          $(".Marker_text2").addClass("Marker_text_active");
+        } else {
+          $(".Marker_circle2").removeClass("active");
+          $(".Marker_text2").removeClass("Marker_text_active");
+        }
+      });
+    }
+  });
+
+  $(function() {
+    $(window).scroll(function() {
+      updateNav3();
+    });
+
+    updateNav3();
+
+    function updateNav3() {
+      $(".Three").each(function(index) {
+        var topOffset = $(this).offset().top;
+        if (topOffset - $(window).scrollTop() < 50) {
+          $(".Marker_circle2").removeClass("active");
+          $(".Marker_text2").removeClass("Marker_text_active");
+
+          $(".Marker_circle3").addClass("active");
+          $(".Marker_text3").addClass("Marker_text_active");
+        } else {
+          $(".Marker_circle3").removeClass("active");
+          $(".Marker_text3").removeClass("Marker_text_active");
+        }
+      });
+    }
+  });
+
   return (
     <div className="Rightsidebar_wrapper">
       <div className="Rightsidebar_padding">
         <div className="Rightsidebar_navigator">
           <div className="Marker">
             <div className="Marker_div">
-              <p className="Marker_text">Home</p>
+              <p className="Marker_text1">Home</p>
             </div>
 
             <div className="dots">
-              <img src={Circle} alt="" className="Marker_circle"></img>
+              <div className="Marker_circle1"></div>
+              <span class="whitedot"></span>
+            </div>
+          </div>
+          
+          <div className="Marker">
+            <div className="Marker_div">
+              <p className="Marker_text2">About</p>
+            </div>
+            <div className="dots">
+            <div className="Marker_circle2"></div>              
+            <span class="whitedot"></span>
+            </div>
+          </div>
+          <div className="Marker">
+            <div className="Marker_div">
+              <p className="Marker_text3">Projects</p>
+            </div>
+            <div className="dots">
+            <div className="Marker_circle3"></div> 
               <span class="whitedot"></span>
             </div>
           </div>
           <div className="Marker">
             <div className="Marker_div">
-              <p className="Marker_text">About</p>
+              <p className="Marker_text4">Services</p>
             </div>
             <div className="dots">
-              <img src={Circle} alt="" className="Marker_circle"></img>
-              <span class="whitedot"></span>
-            </div>
-          </div>
-          <div className="Marker">
-            <div className="Marker_div">
-              <p className="Marker_text">Projects</p>
-            </div>
-            <div className="dots">
-              <img src={Circle} alt="" className="Marker_circle"></img>
-              <span class="whitedot"></span>
-            </div>
-          </div>
-          <div className="Marker">
-            <div className="Marker_div">
-              <p className="Marker_text">Services</p>
-            </div>
-            <div className="dots">
-              <img src={Circle} alt="" className="Marker_circle"></img>
+
               <span class="whitedot"></span>
             </div>
           </div>
@@ -61,6 +135,7 @@ function Rightsidebar() {
                 animation={arrowDown}
                 strokeColor="#2CE6FF"
                 className="Arrow_down"
+                size={45}
               />
             </div>
           </div>
